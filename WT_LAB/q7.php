349d41229392011c12c45dc62c778a51fbc4e385
Motassim Khan <motassimkhan4@gmail.com>
2025-06-16 00:24:10 +0530

    small changes

--- WT_LAB/q7.php
+++ WT_LAB/q7.php
@@ -1,15 +1,9 @@
 <?php 
-// Input file path 
 $inputFile = 'input.txt'; 
-// Output file path 
 $outputFile = 'output.txt'; 
-// Check if the input file exists 
 if (file_exists($inputFile)) { 
-// Read the data from the input file 
 $data = file_get_contents($inputFile); 
-// Check if the data was read successfully 
 if ($data !== false) { 
-// Write the data to the output file 
 $writeSuccess = file_put_contents($outputFile, $data); 
 if ($writeSuccess !== false) { 
 echo "Data successfully written to $outputFile\n"; 
